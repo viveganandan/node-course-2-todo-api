c194c06c21597cb29d4e82e67f8aa192ae1064d4
viveganandan <middaysundonkey@gmail.com>
2018-01-19 17:22:26 -0800

    Get todos

--- server/server.js
+++ server/server.js
@@ -9,15 +9,21 @@ app.use(bodyparser.json());
 
 // Creating a new todo
 app.post('/todos', (req, res) => {
-    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
     todo.save().then((doc) => {
-        console.log(doc);
         res.send(doc);
     }, (err) => {
-        console.log( JSON.stringify(err, undefined, 2));
+        res.status(400).send(err);
+    });
+});
+
+// Get all todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
         res.status(400).send(err);
     });
 });
